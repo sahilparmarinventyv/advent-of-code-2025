c970efe0d0b514741398c5137b6d5951047af642
sahilparmarinventyv <sahil.parmar.inventyv@gmail.com>
2026-01-05 11:55:14 +0530

    initial commit

---
+++ Day1/part1.py
@@ -0,0 +1,27 @@
+input_file = "/home/sahil/Desktop/advent-of-code-2025/day1_part1_input.txt"
+
+with open(input_file,'r') as file:
+    rotations = file.readlines()
+
+for i in range(len(rotations)):
+    
+    if i < len(rotations)-1:
+        rotations[i] = rotations[i][:-1]
+    
+
+# print(rotations)
+curr = 50
+count = 0
+
+for r in rotations:
+    d, s = r[0], int(r[1:])
+
+    if d == 'L':
+        curr = (curr - s + 100) % 100
+    else:
+        curr = (curr + s) % 100
+
+    if curr == 0:
+        count += 1
+
+print(count)
